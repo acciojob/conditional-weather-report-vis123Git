0538194f405e1e218b5c89eddb3b75f07754bd24
VishalKumarSain <vishaldhoria007@gmail.com>
2024-04-16 19:34:05 +0530

    test 2

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function WeatherDisplay({ weather }) {
   const { temperature, conditions } = weather;
 
   // Determine color based on the temperature
   const colorStyle = {
-    color: temperature > 20 ? 'rgb(255, 0, 0)' : 'rgb(0, 0, 255)'
+    color: temperature > 20 ? "rgb(255, 0, 0)" : "rgb(0, 0, 255)",
   };
 
   return (
     <div>
-      <p style={colorStyle}>Temperature: {temperature}</p>
+      <p>
+        <span style={colorStyle}>Temperature:{temperature}</span>
       <span>Conditions: {conditions}</span>
+      </p>
     </div>
   );
 }
